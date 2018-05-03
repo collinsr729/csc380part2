b246d06a853b2c0e1d569aa728c9e376c5c3a3f6
Ryan Collins <collinsr729@yahoo.com>
2018-05-03 10:55:49 -0400

    making panel super complex and stuff
    needs work

--- src/main/java/csc380/myFrame.java
+++ src/main/java/csc380/myFrame.java
@@ -1,6 +1,8 @@
 package csc380;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,27 +13,30 @@ import javax.swing.JTextPane;
 import javax.swing.text.JTextComponent;
 
 public class myFrame extends JFrame {
-	JTextPane text;
+	JTextPane text, textBox,textFeed;
 	JButton confirm, deny;
 	boolean bool = true;
 	boolean buttonPressed = false;
-	JPanel panel;
+	JPanel leftOkCancel;
 	public myFrame() {
 		super("Campus Delivery");
 
+		setBounds(100, 100, 800, 400);
 		confirm = new JButton("Ok");
 		deny = new JButton("Cancel");
 
 		text = new JTextPane();
-		text.setEditable(true);
+		text.setEditable(false);
+		textBox = new JTextPane();
+		textBox.setEditable(true);
+		textFeed = new JTextPane() {
+			public void addItem(String s) {
+				this.setText(this.getText()+"\n"+s);
+			}
+		};
+		text.setEditable(false);
 		text.setText("Just setting up...");
-		setBounds(100, 100, 
-				Math.max(text.getWidth(),confirm.getWidth()+deny.getWidth()),
-				20+text.getHeight()+confirm.getHeight());
-		panel = new JPanel();
-		panel.setBounds(getBounds());
-		panel.setLayout(new BorderLayout());
-		panel.add(text,BorderLayout.PAGE_START);
+		
 		
 		confirm.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -39,15 +44,88 @@ public class myFrame extends JFrame {
 				buttonPressed = true;
 			}
 		});
-		panel.add(confirm, BorderLayout.WEST);
 		deny.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				bool = false;
 				buttonPressed = true;
 			}
 		});
-		panel.add(deny, BorderLayout.EAST);
-		add(panel);
+		
+		leftOkCancel = new JPanel(new BorderLayout());		//Borderlayout to put buttons below text
+		leftOkCancel.setBounds(getX(),getY(),getWidth()/2,getHeight());
+		leftOkCancel.add(text, BorderLayout.NORTH);
+		JPanel leftPanel = new JPanel(new CardLayout());	//Card layout to have multiple variations on the left side
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(confirm);
+		buttonPanel.add(deny);
+		leftOkCancel.add(buttonPanel, BorderLayout.SOUTH);
+		leftPanel.add(leftOkCancel);
+		
+		JPanel rightSide = new JPanel(new CardLayout());
+		JPanel menu = new JPanel();
+	    menu.setLayout(new GridLayout(3, 2));
+	    JButton CheesePizza = new JButton("Cheese Pizza");
+	    CheesePizza.addActionListener(new ActionListener() { 
+	    	  public void actionPerformed(ActionEvent e){ 
+	    		    textFeed.setText(textFeed.getText()+"\nCheese Pizza");
+	    		  } 
+	    		} );
+	    JButton PepPizza = new JButton("Pepperoni Pizza");
+	    PepPizza.addActionListener(new ActionListener() { 
+	    	  public void actionPerformed(ActionEvent e) { 
+	    		    textFeed.setText(textFeed.getText()+"\nCheese Pizza");
+	    		  } 
+	    		} );
+	    JButton Steak = new JButton("Steak");
+	    Steak.addActionListener(new ActionListener() { 
+	    	  public void actionPerformed(ActionEvent e){ 
+	    		    textFeed.setText(textFeed.getText()+"\nCheese Pizza");
+	    		  } 
+	    		} );
+	    JButton Burger = new JButton("Burger");
+	    Burger.addActionListener(new ActionListener() { 
+	    	  public void actionPerformed(ActionEvent e) { 
+	    		    textFeed.setText(textFeed.getText()+"\nCheese Pizza");
+	    		  } 
+	    		} );
+	    JButton Fries = new JButton("Fries");
+	    Fries.addActionListener(new ActionListener() { 
+	    	  public void actionPerformed(ActionEvent e) { 
+	    		    textFeed.setText(textFeed.getText()+"\nCheese Pizza");
+	    		  } 
+	    		} );
+	    JButton CBurger = new JButton("Cheese Burger");
+	    CBurger.addActionListener(new ActionListener() { 
+	    	  public void actionPerformed(ActionEvent e) { 
+	    		    textFeed.setText(textFeed.getText()+"\nCheese Pizza");
+	    		  } 
+	    		} );
+	    
+	    JButton Checkout = new JButton("Checkout");
+	    Checkout.addActionListener(new ActionListener() { 
+	    	  public void actionPerformed(ActionEvent e) { 
+	    		    textFeed.setText(textFeed.getText()+"\nCheese Pizza");
+	    		    ///////// NEED TO INSPECT textFeed here - create a menu
+	    		  } 
+	    		} );
+	    menu.add(CheesePizza);
+	    menu.add(PepPizza);
+	    menu.add(CBurger);
+	    menu.add(Burger);
+	    menu.add(Fries);
+	    menu.add(Steak);
+	    menu.add(Checkout);
+		menu.setVisible(true);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		add(leftPanel);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
